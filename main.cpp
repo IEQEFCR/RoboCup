827e9f875f1f310f31ec13e85f68ddc015704f65
Kai <ieqefcr@hotmail.com>
2024-11-24 22:55:08 +0800

    bb add new line

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@ int main() {
     // std::cout << "Hello, RoboCup!" << std::endl;
     // main add a new line
     // add a new line
-    b add a new line
+    b add a new line;
+    b add a new line;
 }
