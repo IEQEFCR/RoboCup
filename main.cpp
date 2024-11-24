4f7241bd26f3271cee6532bbec1dfbd3891d4c60
Kai <ieqefcr@hotmail.com>
2024-11-24 23:19:07 +0800

    b add a new fuc

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@ void newFunction() {
     printf("This is a new function\n");
 }
 
+void b() {
+    printf("b\n");
+}
+
 int main() {
     // std::cout << "Hello, RoboCup!" << std::endl;
     // main add a new line
